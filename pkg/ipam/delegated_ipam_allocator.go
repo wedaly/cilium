cc64925ae81ef97db822fe0e2a3ac36754ff76fc
Will Daly <widaly@microsoft.com>
2023-11-09 21:32:03 +0000

    hack: fake store for delegated IPAM state

--- pkg/ipam/delegated_ipam_allocator.go
+++ pkg/ipam/delegated_ipam_allocator.go
@@ -6,6 +6,9 @@ package ipam
 import (
 	"fmt"
 	"net"
+	"sync"
+
+	"github.com/google/uuid"
 )
 
 // delegatedIPAMAllocator implements ipam.Allocator that invokes a delegated IPAM CNI plugin.
@@ -61,4 +64,79 @@ func (d *delegatedIPAMAllocator) Capacity() uint64 {
 
 func (d *delegatedIPAMAllocator) RestoreFinished() {
 	// Delegated IPAM doesn't do anything on restore finished.
+}
+
+// delegatedIPAMStore maintains state of delegated IPAM allocations.
+// TODO: for the prototype, keep this in-memory, but in a real implementation
+// we'd need to persist to disk to avoid leaking IPs on cilium-agent restart.
+type delegatedIPAMStore struct {
+	mu       sync.Mutex
+	entryMap map[delegatedIPAMKey]*delegatedIPAMEntry
+}
+
+type delegatedIPAMKey struct {
+	containerID string
+	ifname      string
+}
+
+type delegatedIPAMEntry struct {
+	ipv4 net.IP
+	ipv6 net.IP
+}
+
+func newDelegatedIPAMStore() *delegatedIPAMStore {
+	return &delegatedIPAMStore{
+		entryMap: make(map[delegatedIPAMKey]*delegatedIPAMEntry),
+	}
+}
+
+func (s *delegatedIPAMStore) startAllocation() (delegatedIPAMKey, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// Choose UUID containerID to avoid conflict with IDs assigned by the container runtime.
+	key := delegatedIPAMKey{
+		containerID: uuid.New().String(),
+		ifname:      "", // TODO: not sure if this needs to be set.
+	}
+	s.entryMap[key] = nil
+	return key, nil
+}
+
+func (s *delegatedIPAMStore) completeAllocation(key delegatedIPAMKey, ipv4 net.IP, ipv6 net.IP) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_, ok := s.entryMap[key]
+	if !ok {
+		return fmt.Errorf("Could not find delegated IPAM entry for key %s", key)
+	}
+
+	s.entryMap[key] = &delegatedIPAMEntry{
+		ipv4: ipv4,
+		ipv6: ipv6,
+	}
+
+	return nil
+}
+
+func (s *delegatedIPAMStore) lookupIP(ip net.IP) (*delegatedIPAMKey, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for key, entry := range s.entryMap {
+		if entry.ipv4.Equal(ip) || entry.ipv6.Equal(ip) {
+			return &key, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func (s *delegatedIPAMStore) deleteAllocation(key delegatedIPAMKey) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	delete(s.entryMap, key)
+	return nil
 }
