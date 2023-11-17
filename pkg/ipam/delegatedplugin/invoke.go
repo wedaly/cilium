3157dd31b495ef6218c00180b2864fabe97d5d2a
Will Daly <widaly@microsoft.com>
2023-11-17 11:33:36 -0800

    fixup: preserve ipam when injecting to netconf

--- pkg/ipam/delegatedplugin/invoke.go
+++ pkg/ipam/delegatedplugin/invoke.go
@@ -2,13 +2,12 @@ package delegatedplugin
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"os"
+	"path"
 	"strings"
 
+	"github.com/containernetworking/cni/libcni"
 	cniInvoke "github.com/containernetworking/cni/pkg/invoke"
-	cniTypes "github.com/containernetworking/cni/pkg/types"
 	cniTypesV1 "github.com/containernetworking/cni/pkg/types/100"
 )
 
@@ -39,26 +38,21 @@ func (a *cniArgs) AsEnv() []string {
 // TODO: this should probably be an interface so we can mock it in tests.
 type Invoker struct {
 	cniBinaryPaths []string // Equivalent of CNI_PATH env var in CNI plugin
-	netConf        *cniTypes.NetConf
+	netConf        *libcni.NetworkConfig
 }
 
 func NewInvoker(cniConflistPath string, cniBinaryPaths []string) (*Invoker, error) {
 	// TODO: add retries or watcher in case conflist doesn't yet exist.
-	data, err := os.ReadFile(cniConflistPath)
+	dir, name := path.Split(cniConflistPath)
+	netConfList, err := libcni.LoadConfList(dir, name)
 	if err != nil {
-		return nil, fmt.Errorf("Error reading CNI conflist from %q: %w", cniConflistPath, err)
-	}
-
-	// Parse it, so we fail fast if it's broken. And so we don't need to parse it again.
-	var netConfList cniTypes.NetConfList
-	if err := json.Unmarshal(data, &netConfList); err != nil {
-		return nil, fmt.Errorf("Error parsing CNI conflist from %q: %w", cniConflistPath, err)
+		return nil, fmt.Errorf("Error loading CNI conflist from %q: %w", cniConflistPath, err)
 	}
 
 	// Better find Cilium in the conflist.
-	var netConf *cniTypes.NetConf
+	var netConf *libcni.NetworkConfig
 	for _, plugin := range netConfList.Plugins {
-		if plugin.Type == "cilium-cni" {
+		if plugin.Network.Type == "cilium-cni" {
 			netConf = plugin
 			break
 		}
@@ -66,16 +60,21 @@ func NewInvoker(cniConflistPath string, cniBinaryPaths []string) (*Invoker, erro
 
 	if netConf == nil {
 		return nil, fmt.Errorf("Could not find Cilium plugin in CNI conflist %s", cniConflistPath)
-	} else if netConf.IPAM.Type == "" {
+	} else if netConf.Network.IPAM.Type == "" {
 		return nil, fmt.Errorf("Cilium CNI config does not have specify delegated IPAM plugin")
 	}
 
 	// Need to copy some values from the conflist to the runtime config.
 	// https://www.cni.dev/docs/spec/#deriving-runtimeconfig
 	// We're doing the bare minimum here to get this working.
-	netConf.CNIVersion = netConfList.CNIVersion
-	netConf.Name = netConfList.Name
-	netConf.Capabilities = nil
+	inject := map[string]interface{}{
+		"name":       netConfList.Name,
+		"cniVersion": netConfList.CNIVersion,
+	}
+	netConf, err = libcni.InjectConf(netConf, inject)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to inject params to CNI config: %w", err)
+	}
 
 	invoker := &Invoker{
 		cniBinaryPaths: cniBinaryPaths,
@@ -94,17 +93,12 @@ func (in *Invoker) DelegateAdd(ctx context.Context, containerId string) (*cniTyp
 		CNIPath:     strings.Join(in.cniBinaryPaths, ":"),
 	}
 
-	pluginPath, err := cniInvoke.FindInPath(in.netConf.IPAM.Type, in.cniBinaryPaths)
+	pluginPath, err := cniInvoke.FindInPath(in.netConf.Network.IPAM.Type, in.cniBinaryPaths)
 	if err != nil {
 		return nil, err
 	}
 
-	netConfData, err := json.Marshal(in.netConf)
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := cniInvoke.ExecPluginWithResult(ctx, pluginPath, netConfData, args, nil)
+	result, err := cniInvoke.ExecPluginWithResult(ctx, pluginPath, in.netConf.Bytes, args, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -125,17 +119,12 @@ func (in *Invoker) DelegateDelete(ctx context.Context, containerId string) error
 		CNIPath:     strings.Join(in.cniBinaryPaths, ":"),
 	}
 
-	pluginPath, err := cniInvoke.FindInPath(in.netConf.IPAM.Type, in.cniBinaryPaths)
+	pluginPath, err := cniInvoke.FindInPath(in.netConf.Network.IPAM.Type, in.cniBinaryPaths)
 	if err != nil {
 		return err
 	}
 
-	netConfData, err := json.Marshal(in.netConf)
-	if err != nil {
-		return err
-	}
-
-	return cniInvoke.ExecPluginWithoutResult(ctx, pluginPath, netConfData, args, nil)
+	return cniInvoke.ExecPluginWithoutResult(ctx, pluginPath, in.netConf.Bytes, args, nil)
 }
 
 func (in *Invoker) DelegateCheck(ctx context.Context, containerId string) error {
@@ -147,15 +136,10 @@ func (in *Invoker) DelegateCheck(ctx context.Context, containerId string) error 
 		CNIPath:     strings.Join(in.cniBinaryPaths, ":"),
 	}
 
-	pluginPath, err := cniInvoke.FindInPath(in.netConf.IPAM.Type, in.cniBinaryPaths)
+	pluginPath, err := cniInvoke.FindInPath(in.netConf.Network.IPAM.Type, in.cniBinaryPaths)
 	if err != nil {
 		return err
 	}
 
-	netConfData, err := json.Marshal(in.netConf)
-	if err != nil {
-		return err
-	}
-
-	return cniInvoke.ExecPluginWithoutResult(ctx, pluginPath, netConfData, args, nil)
+	return cniInvoke.ExecPluginWithoutResult(ctx, pluginPath, in.netConf.Bytes, args, nil)
 }
